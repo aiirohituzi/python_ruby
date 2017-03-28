f26eb3853141e7e023ec05fc3c5a330b510f3600
aiirohituzi <sinn5656@gmail.com>
2017-03-28 23:34:14 +0900

    Merge develop
    study 6 - module

---
+++ Module/3.py
@@ -0,0 +1,8 @@
+# import egoing
+from egoing import a as z   # egoing 이라는 모듈의 a만을 import해서 이름을 z로 명명
+import k8805 as k           # k8805 라는 모듈을 import하여 k라는 이름으로 사용
+# import egoing, k8805
+
+# print(egoing.a())
+print(z())
+print(k.a())
---
+++ Module/4_2.py
@@ -0,0 +1,9 @@
+# def a():
+#     return 'a'
+# print(a())
+
+# 4_1.py와 중복이 발생
+
+# 모듈을 통한 중복의 제거 & 재활용성 증대
+import egoing
+print(egoing.a())
