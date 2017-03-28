7f45386d3881bade98a39dc242a14a161e597d5c
aiirohituzi <sinn5656@gmail.com>
2017-03-28 23:36:18 +0900

    Merge develop
    study 6 - module

---
+++ Module/1.py
@@ -0,0 +1,4 @@
+import math     # 내장 모듈
+print(math.ceil(2.9))
+print(math.floor(2.9))
+print(math.sqrt(16))
---
+++ Module/2.py
@@ -0,0 +1,15 @@
+def a():
+    return 'a'
+#엄청 많은 코드
+def a():
+    return 'B'
+#엄청 많은 코드
+print(a())      # 협업중 덮어씌워버릴 우려
+
+def egoing_a():
+    return 'a'
+#엄청 많은 코드
+def k8805_a():
+    return 'B'
+#엄청 많은 코드
+print(egoing_a())       # 구분은 되나 좋지 않은 방법
