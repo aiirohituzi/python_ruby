2e44d8352689b0ceea8e8d2c0bd2973eff4e608f
aiirohituzi <sinn5656@gmail.com>
2017-03-29 22:34:56 +0900

    Delete django directory

--- django/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sinn5\documents\github\python\django\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
